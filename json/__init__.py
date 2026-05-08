b7a38848f907cac3181a380dfc4b3e7e548f452a
svivek30 <109782044+svivek30@users.noreply.github.com>
2026-05-09 00:41:25 +0530

    Add files via upload

--- json/__init__.py
+++ json/__init__.py
@@ -1,15 +1,7 @@
-from pandas.io.json._json import (
-    read_json,
-    to_json,
-    ujson_dumps,
-    ujson_loads,
+from pandas.tests.extension.json.array import (
+    JSONArray,
+    JSONDtype,
+    make_data,
 )
-from pandas.io.json._table_schema import build_table_schema
 
-__all__ = [
-    "ujson_dumps",
-    "ujson_loads",
-    "read_json",
-    "to_json",
-    "build_table_schema",
-]
+__all__ = ["JSONArray", "JSONDtype", "make_data"]
