7e68c37a51e04029038eb4a9891e729f013aeefa
svivek30 <109782044+svivek30@users.noreply.github.com>
2026-05-09 00:33:58 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,27 +1,13 @@
-__all__ = [
-    "NaT",
-    "NaTType",
-    "OutOfBoundsDatetime",
-    "Period",
-    "Timedelta",
-    "Timestamp",
-    "iNaT",
-    "Interval",
-]
+# ruff: noqa: TCH004
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    # import modules that have public classes/functions
+    from pandas.io import (
+        formats,
+        json,
+        stata,
+    )
 
-# Below imports needs to happen first to ensure pandas top level
-# module gets monkeypatched with the pandas_datetime_CAPI
-# see pandas_datetime_exec in pd_datetime.c
-import pandas._libs.pandas_parser  # isort: skip # type: ignore[reportUnusedImport]
-import pandas._libs.pandas_datetime  # noqa: F401 # isort: skip # type: ignore[reportUnusedImport]
-from pandas._libs.interval import Interval
-from pandas._libs.tslibs import (
-    NaT,
-    NaTType,
-    OutOfBoundsDatetime,
-    Period,
-    Timedelta,
-    Timestamp,
-    iNaT,
-)
+    # mark only those modules as public
+    __all__ = ["formats", "json", "stata"]
